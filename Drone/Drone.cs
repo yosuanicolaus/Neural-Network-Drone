6905eee0b03aa46c217ca69abb82dcefd6e838cc
Yosua Nicolaus <cryodawn257@gmail.com>
2022-06-08 00:32:45 +0700

    Activate Brain(AI) control over Drone

--- Drone/Drone.cs
+++ Drone/Drone.cs
@@ -3,9 +3,8 @@ using System;
 
 public class Drone : RigidBody2D
 {
-    [Export] float ThrusterAngularSpeed = 240f;
-    [Export] float ThrusterForce = 300f;
-    // [Export] float ForceLimit = 100f;
+    float ThrusterAngularSpeed = 240f;
+    float ThrusterForce = 300f;
     private float _maxRotationDegree = 75f;
 
     public int Point = 0;
@@ -15,6 +14,7 @@ public class Drone : RigidBody2D
     Vector2 RightDirection = new Vector2();
     Vector2 LeftForce = new Vector2();
     Vector2 RightForce = new Vector2();
+    Vector2 Velocity = new Vector2();
 
     Node2D LeftThruster;
     Node2D RightThruster;
@@ -22,8 +22,13 @@ public class Drone : RigidBody2D
     Node2D RTTarget;
     Node2D TargetPoint;
 
+    NeuralNetwork Brain = new NeuralNetwork(new int[] { 8, 10, 10, 4 });
+    double[] Inputs = new double[8];
+    double[] Outputs = new double[4];
+
     public override void _Ready()
     {
+        Position = GetViewportRect().Size / 2;
         LeftThruster = GetNode<Node2D>("Left");
         RightThruster = GetNode<Node2D>("Right");
         LTTarget = GetNode<Node2D>("Left/Target");
@@ -32,14 +37,49 @@ public class Drone : RigidBody2D
 
     public override void _PhysicsProcess(float delta)
     {
-        GetRotations(delta);
+        FeedBrain(delta);
+        Activate(delta);
+        ClampDegrees();
         GetDirections(delta);
+    }
 
-        if (Input.IsActionPressed("f"))
+    void FeedBrain(float delta)
+    {
+        Inputs[0] = TargetPoint.Position.x - Position.x;
+        Inputs[1] = TargetPoint.Position.y - Position.y;
+        Inputs[2] = LinearVelocity.x;
+        Inputs[3] = LinearVelocity.y;
+        Inputs[4] = AngularVelocity;
+        Inputs[5] = LeftThruster.GlobalRotation;
+        Inputs[6] = RightThruster.GlobalRotation;
+        Inputs[7] = GlobalRotation;
+
+        Outputs = Brain.FeedForward(Inputs);
+    }
+
+    void Activate(float delta)
+    {
+        if (Outputs[0] > 0.5)
+        {
+            LeftThruster.RotationDegrees += ThrusterAngularSpeed * delta;
+        }
+        else
+        {
+            LeftThruster.RotationDegrees -= ThrusterAngularSpeed * delta;
+        }
+        if (Outputs[1] > 0.5)
+        {
+            RightThruster.RotationDegrees += ThrusterAngularSpeed * delta;
+        }
+        else
+        {
+            RightThruster.RotationDegrees -= ThrusterAngularSpeed * delta;
+        }
+        if (Outputs[2] > 0.5)
         {
             ApplyImpulse(LeftThruster.Position, LeftForce);
         }
-        if (Input.IsActionPressed("j"))
+        if (Outputs[3] > 0.5)
         {
             ApplyImpulse(RightThruster.Position, RightForce);
         }
@@ -57,27 +97,12 @@ public class Drone : RigidBody2D
         RightForce = RightDirection * ThrusterForce * delta;
     }
 
-    void GetRotations(float delta)
+    void ClampDegrees()
     {
-        if (Input.IsActionPressed("s"))
-        {
-            LeftThruster.RotationDegrees += ThrusterAngularSpeed * delta;
-        }
-        if (Input.IsActionPressed("d"))
-        {
-            LeftThruster.RotationDegrees -= ThrusterAngularSpeed * delta;
-        }
-        if (Input.IsActionPressed("k"))
-        {
-            RightThruster.RotationDegrees += ThrusterAngularSpeed * delta;
-        }
-        if (Input.IsActionPressed("l"))
-        {
-            RightThruster.RotationDegrees -= ThrusterAngularSpeed * delta;
-        }
-
-        LeftThruster.RotationDegrees = Mathf.Clamp(LeftThruster.RotationDegrees, -_maxRotationDegree, _maxRotationDegree);
-        RightThruster.RotationDegrees = Mathf.Clamp(RightThruster.RotationDegrees, -_maxRotationDegree, _maxRotationDegree);
+        LeftThruster.RotationDegrees = Mathf.Clamp
+        (LeftThruster.RotationDegrees, -_maxRotationDegree, _maxRotationDegree);
+        RightThruster.RotationDegrees = Mathf.Clamp
+        (RightThruster.RotationDegrees, -_maxRotationDegree, _maxRotationDegree);
     }
 
     public void SetTarget(Node2D target)
