fc7442428307025ec56ac3374c8c428c6c7bcede
Yosua Nicolaus <cryodawn257@gmail.com>
2022-06-07 19:36:44 +0700

    code Drone's variables and controls
    temporarily disable gravity for testing

--- Drone/Drone.cs
+++ Drone/Drone.cs
@@ -3,19 +3,70 @@ using System;
 
 public class Drone : RigidBody2D
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+    [Export] float ThrusterAngularSpeed = 240f;
+    [Export] float ThrusterForce = 300f;
+    [Export] float ForceLimit = 100f;
+    private float _maxRotationDegree = 75f;
 
-    // Called when the node enters the scene tree for the first time.
+    Vector2 LeftDirection = new Vector2();
+    Vector2 RightDirection = new Vector2();
+    Vector2 LeftForce = new Vector2();
+    Vector2 RightForce = new Vector2();
+
+    Node2D LeftThruster;
+    Node2D RightThruster;
+    Node2D LTTarget;
+    Node2D RTTarget;
+
     public override void _Ready()
     {
-
+        LeftThruster = GetNode<Node2D>("Left");
+        RightThruster = GetNode<Node2D>("Right");
+        LTTarget = GetNode<Node2D>("Left/Target");
+        RTTarget = GetNode<Node2D>("Right/Target");
     }
 
-    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
-    //  public override void _Process(float delta)
-    //  {
-    //      
-    //  }
+    public override void _PhysicsProcess(float delta)
+    {
+        GetDirections(delta);
+        GetHumanControl(delta);
+
+        // ApplyImpulse(LeftThruster.Position, LeftForce);
+        // ApplyImpulse(RightThruster.Position, RightForce);
+    }
+
+    void GetDirections(float delta)
+    {
+        LeftForce = Vector2.Zero;
+        RightForce = Vector2.Zero;
+
+        LeftDirection = (LTTarget.GlobalPosition - LeftThruster.GlobalPosition).Normalized();
+        RightDirection = (RTTarget.GlobalPosition - RightThruster.GlobalPosition).Normalized();
+
+        LeftForce = LeftDirection * ThrusterForce * delta;
+        RightForce = RightDirection * ThrusterForce * delta;
+    }
+
+    private void GetHumanControl(float delta)
+    {
+        if (Input.IsActionPressed("ui_left"))
+        {
+            LeftThruster.RotationDegrees += ThrusterAngularSpeed * delta;
+        }
+        if (Input.IsActionPressed("ui_right"))
+        {
+            RightThruster.RotationDegrees += ThrusterAngularSpeed * delta;
+        }
+        if (Input.IsActionPressed("ui_up"))
+        {
+            LeftThruster.RotationDegrees -= ThrusterAngularSpeed * delta;
+        }
+        if (Input.IsActionPressed("ui_down"))
+        {
+            RightThruster.RotationDegrees -= ThrusterAngularSpeed * delta;
+        }
+
+        LeftThruster.RotationDegrees = Mathf.Clamp(LeftThruster.RotationDegrees, -_maxRotationDegree, _maxRotationDegree);
+        RightThruster.RotationDegrees = Mathf.Clamp(RightThruster.RotationDegrees, -_maxRotationDegree, _maxRotationDegree);
+    }
 }
