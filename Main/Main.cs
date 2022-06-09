b70970ea7aa0d501b5d72b79597d83a6a3dc39d5
Yosua Nicolaus <cryodawn257@gmail.com>
2022-06-09 09:55:44 +0700

    create DronePopulation for managing Drone's Gens

--- Main/Main.cs
+++ Main/Main.cs
@@ -6,18 +6,18 @@ public class Main : Node2D
     PackedScene droneScene = (PackedScene)ResourceLoader.Load("res://Drone/Drone.tscn");
     PackedScene pointScene = (PackedScene)ResourceLoader.Load("res://Main/Point.tscn");
 
-    Population Drones;
+    DronePopulation Drones;
     Population Points;
     public int Size = 5;
 
     public override void _Ready()
     {
-        Drones = new Population(droneScene, Size);
+        Drones = new DronePopulation(droneScene, Size);
         Points = new Population(pointScene, Size);
 
         for (int i = 0; i < Size; i++)
         {
-            Drone drone = (Drone)Drones.Scenes[i];
+            Drone drone = Drones.DroneScenes[i];
             Point point = (Point)Points.Scenes[i];
             drone.TargetPoint = point;
             drone.Connect("Crashed", this, "onDroneCrashed");
@@ -41,6 +41,8 @@ public class Main : Node2D
         Size--;
         if (Size == 0)
         {
+            Size = Drones.Size;
+            Drones.Reincarnate();
             // GetTree().ReloadCurrentScene();
         }
     }
--- NeuralNetwork/Population.cs
+++ NeuralNetwork/Population.cs
@@ -20,3 +20,79 @@ public class Population : Godot.Object
         }
     }
 }
+
+
+public class DronePopulation : Population
+{
+    public Drone[] DroneScenes;
+    Random rng = new Random();
+    NeuralNetwork[] brains;
+
+    public DronePopulation(PackedScene scene, int size) : base(scene, size)
+    {
+        DroneScenes = new Drone[size];
+        brains = new NeuralNetwork[size];
+        for (int i = 0; i < size; i++)
+        {
+            DroneScenes[i] = (Drone)Scenes[i];
+            brains[i] = DroneScenes[i].Brain;
+        }
+    }
+
+    void Selection()
+    {
+
+    }
+
+    void UpdateBrains()
+    {
+
+    }
+
+    public void Reincarnate()
+    {
+        Selection();
+        UpdateBrains();
+    }
+
+    // void Selection()
+    // {
+    //     float totalScore = 0f;
+    //     float[] scores = new float[Size];
+
+    //     foreach (Node scene in Scenes)
+    //     {
+    //         var drone = (Drone)scene;
+    //         drone.CalculateScore();
+    //         totalScore += drone.Score;
+    //     }
+
+    // }
+
+    // NeuralNetwork PoolSelection(float[] scores, float totalScore)
+    // {
+    //     int i = -1;
+    //     float r = (float)rng.NextDouble() * totalScore;
+
+    //     while (r > 0)
+    //     {
+    //         i++;
+    //         r -= scores[i];
+    //     }
+
+    //     return brains[i];
+    // }
+
+    // public void Reincarnate()
+    // {
+    //     Selection();
+
+    //     for (int i = 0; i < Size; i++)
+    //     {
+    //         Scenes[i].QueueFree();
+    //         Scenes[i] = (Node)_scene.Instance();
+    //         Drone drone = (Drone)Scenes[i];
+    //         drone.Brain = brains[i];
+    //     }
+    // }
+}
